51773aab0e22b7f2b11f2f02f4b4444a156e1118
Thorsten Knöller <thorsten.knoeller@immundiagnostik.com>
2018-10-02 16:21:27 +0200

    ADD/translations: list of translations

---
+++ src/String.js
@@ -0,0 +1,136 @@
+import {Component} from "react";
+import React from "react";
+
+function TranslationString(props) {
+    var translation = props.translation || null;
+    if (translation) {
+        return translation.Translation
+    }
+    return ""
+}
+
+function Approved(props) {
+    var translation = props.translation || null;
+    if (translation) {
+        return translation.Approved
+    }
+    return ""
+}
+
+function NeedsImprovement(props) {
+    var translation = props.translation || null;
+    if (translation) {
+        return translation.NeedsImprovement
+    }
+    return ""
+}
+
+class String extends Component {
+    constructor() {
+        super();
+        this.state = {
+            isCreated: false,
+            identifier: {Id: "", Identifier: ""},
+            translation: {Translation: "", Approved: false, NeedsImprovement: false},
+            language: ""
+        };
+
+        this.handleTranslationChange = this.handleTranslationChange.bind(this);
+        this.updateTranslation = this.updateTranslation.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            "identifier": this.props.identifier,
+            "language": this.props.language
+        });
+        if (this.props.identifier != null && this.props.identifier.Translations != null) {
+            for (var i = 0; i < this.props.identifier.Translations.length; i++) {
+                let translation = this.props.identifier.Translations[i];
+                if (translation.Language === this.props.language) {
+                    this.setState({
+                        "translation": translation
+                    });
+                }
+            }
+        }
+
+    }
+
+    baseTranslation() {
+        if (this.state.identifier == null || this.state.identifier.Translations == null) {
+            return null;
+        }
+        for (var i = 0; i < this.state.identifier.Translations.length; i++) {
+            let translation = this.state.identifier.Translations[i];
+            if (translation.Language === this.props.baseLanguage) {
+                return translation;
+            }
+        }
+        return null;
+    }
+
+
+    string() {
+        var translation = translation();
+        if (translation) {
+            return translation.Translation
+        }
+        return ""
+    }
+
+    handleTranslationChange(event) {
+        const target = event.target;
+        const value = target.value;
+
+        this.setState({
+            "translation": {"Translation": value}
+        });
+    }
+
+    updateTranslation() {
+        var body = {
+            "keyId": this.state.identifier.Id,
+            "translation": this.state.translation.Translation,
+            "languageCode": this.state.language
+        };
+        console.log(body)
+        fetch('http://localhost:8080/translation', {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(body)
+        })
+            .then(result => {
+                if (!result.ok) {
+                    throw Error(result.statusText);
+                }
+                return result.json()
+            })
+            .then(result => {
+                this.state.languages.push(result);
+                this.setState({
+                    "languages": this.state.languages
+                });
+            })
+            .catch((error) => {
+                console.log(error)
+            });
+    }
+
+    render() {
+        return (
+            <tr key={this.state.identifier.Id}>
+                <td>{this.state.identifier.Identifier}</td>
+                <td><TranslationString translation={this.baseTranslation()}/></td>
+                <td><Approved translation={this.translation}/></td>
+                <td><NeedsImprovement translation={this.translation}/></td>
+                <td><textarea name="translationString" value={this.state.translation.Translation} onChange={this.handleTranslationChange}/></td>
+                <td>
+                    <button onClick={this.updateTranslation}>Update</button>
+                </td>
+            </tr>
+        )
+    }
+}
+
+export default String
--- src/StringList.js
+++ src/StringList.js
@@ -1,6 +1,6 @@
 import {Component} from "react";
 import React from "react";
-
+import String from "./String"
 /**
  * @return {string}
  */
@@ -14,29 +14,7 @@ function TranslationLanguage(props) {
     return ""
 }
 
-/**
- * @return {string}
- */
-function String(props) {
-    if (props.identifier.Translations == null) {
-        return ""
-    }
-    for (var i = 0; i < props.identifier.Translations.length; i++) {
-        let translation = props.identifier.Translations[i];
-        if (translation.Language === props.language) {
-            return translation.Translation
-        }
-    }
-    return ""
-}
-
 class StringList extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentDidMount() {
-    }
 
     render() {
         return (
@@ -46,18 +24,15 @@ class StringList extends Component {
                     <tr>
                         <th>Identifier</th>
                         <th>String {this.props.project.BaseLanguage.Name}</th>
+                        <th>Approved</th>
+                        <th>Improvement needed</th>
                         <th>Translation <TranslationLanguage project={this.props.project}
                                                              language={this.props.language}/></th>
                     </tr>
                     </thead>
                     <tbody>
                     {this.props.project.Identifiers.map(identifier =>
-                        <tr key={identifier.Id}>
-                            <td>{identifier.Identifier}</td>
-                            <td><String language={this.props.project.BaseLanguage.IsoCode} identifier={identifier}/>
-                            </td>
-                            <td><String language={this.props.language} identifier={identifier}/></td>
-                        </tr>
+                        <String identifier={identifier} language={this.props.language} baseLanguage={this.props.project.BaseLanguage.IsoCode}/>
                     )}
                     </tbody>
                 </table>
